c163de791e856571eff5bcb71607d1e3a7847f0d
Khurshed-M <master.khurshed@gmail.com>
2023-02-04 14:46:26 +0300

    frirst commit- everything is alright

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = ["Cook food", "Eat food"];
+
+
+app.get('/', (req, res) => {
+    res.render('list', {newListItem: items});
+});
+app.post('/', (req, res) => {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect('/');
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
