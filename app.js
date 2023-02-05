567a1083194be31f2cfccc43bc94154f09f3c632
Khurshed-M <master.khurshed@gmail.com>
2023-02-05 22:54:55 +0300

    the project is complete except for 'adding empty fields'

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-var items = ["Cook food", "Eat food"];
-
+var items = [];
 
 app.get('/', (req, res) => {
     res.render('list', {newListItem: items});
 });
 app.post('/', (req, res) => {
     var item = req.body.newItem;
-    items.push(item);
+    var button = req.body.btnClick;
+
+    if (button === "addNewItem"){
+        items.push(item);
+    } else {
+        for (let i=0; i<items.length; i++) {
+            if (button === "deleteItem"+i){
+                items.splice(i, 1);
+            }
+        }
+    }
+        
     res.redirect('/');
 });
-
-// app.post('/', (req, res) => {
-//     var num = req.body.numDelete;
-//     // items.slice(num, 1);
-//     console.log(num);
-//     res.redirect('/');
-// });
-
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
